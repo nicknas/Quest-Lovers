e6ddd0166c7d66d256cc7118a1dde475ca0a6570
manuel-freire <manuel.freire@fdi.ucm.es>
2017-03-24 12:04:12 +0100

    Ejemplo minimo de uso de bootstrap

--- src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -2,13 +2,24 @@ package es.ucm.fdi.iw.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 
 @Controller	
 public class RootController {
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("s", "/static");
+    }
 
 	@GetMapping({"/", "/index"})
 	String root() {
 		return "test";
 	}
 	
+	@GetMapping("/b")
+	String boot() {
+		return "boot";
+	}
 }
